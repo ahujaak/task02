eac53dba934037909dc3daf411e7b4ba3413d7b1
ahujaak <akahuja.81@gmail.com>
2020-02-21 02:28:58 +0530

    More func added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+	skip_before_action :verify_authenticity_token, if: -> { request.format.json? || request.format.xml? || request.format.js? }
 	before_action :authenticate_user!
 end
