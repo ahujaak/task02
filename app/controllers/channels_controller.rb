ee4573d187048909c7f40d36ec00bc3e46e3e5da
ahujaak <akahuja.81@gmail.com>
2020-02-21 02:22:56 +0530

    More func added

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -3,4 +3,11 @@ class ChannelsController < ApplicationController
 	def index
 		@channels = Channel.search(params[:search])
 	end
+
+	def add_to_favourite
+		show_ids = current_user.show_ids
+		show_ids << params[:show_id]
+		show_ids = show_ids.flatten
+		current_user.update(show_ids: show_ids)
+	end
 end
